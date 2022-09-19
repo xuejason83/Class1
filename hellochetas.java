5780e60a2fcc95a76d2f6554d345212ebdeff44d
Kevin Hong <kevin@AdministratordeMacBook-Air.local>
2022-09-18 21:30:03 -0400

    SuSurya

---
+++ hellochetas.java
@@ -0,0 +1,10 @@
+package firstjava;
+
+public class hellochetas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("chetasbetasuryamaxwell");
+	}
+
+}
