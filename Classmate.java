4fb06495e06f04e6493ad7fbe8ceaa12f4b1fb94
Lucas Dai <85909101+Lucasdai190@users.noreply.github.com>
2022-10-02 19:19:23 -0400

    Add files via upload

---
+++ Classmate.java
@@ -0,0 +1,19 @@
+public class Classmate {
+
+	public static void main(String[] args) {
+		int age;
+		string full_name;
+		tring class;
+		string grade;
+		string gender;
+		float height;
+		string teacher_name;
+		string skincolour;
+		boolean man;
+		float weight;
+		
+		
+		
+	}
+
+}
