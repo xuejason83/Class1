ca54d7d915800405867632ec652530d09b685e9a
Kevin Hong <kevinhongshenjie@gmail.com>
2022-10-04 20:59:49 -0400

    Identifying_Students

---
+++ MyFirstJavaProject/school.java
@@ -0,0 +1,15 @@
+public class school{
+	public static void main(String[] args)
+	{
+		boolean Male;
+		String Name;
+		int Age;
+		String Skin_Tone;
+		float Weight;
+		String Grade;
+		String Class;
+		String Hobbies;
+		float Hair_Length;
+		boolean Is_Your_Name_Chetas;		
+	}
+}
