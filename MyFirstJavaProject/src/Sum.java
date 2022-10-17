6bb0580a8869fa39a4f78f37415a6f355dc73428
Kevin Hong <kevinhongshenjie@gmail.com>
2022-10-16 21:30:28 -0400

    Sum from 1-188

---
+++ MyFirstJavaProject/src/Sum.java
@@ -0,0 +1,28 @@
+public class Sum {
+	
+	public static void main(String[] args) {
+		int x=188;
+		int result=0;
+		while(x>0){
+			
+			result=result+x;
+			x=x-1;
+		}
+		System.out.print(result);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
