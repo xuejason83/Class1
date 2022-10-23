06a6cc8dea4a7d7c5df40f5a107ffa5bc2c1a7b9
Kevin Hong <kevinhongshenjie@gmail.com>
2022-10-23 11:32:39 -0400

    New sum

--- MyFirstJavaProject/src/Sum.java
+++ MyFirstJavaProject/src/Sum.java
@@ -1,17 +1,20 @@
 public class Sum {
 	
-	public static void main(String[] args) {
-		int x=188;
-		int result=0;
-		while(x>0){
-			
-			result=result+x;
-			x=x-1;
-		}
-		System.out.print(result);
-	}
-}
+    public static void main(String[] args) {
+        int x=1;
+        int sum=0;
+        boolean Printed = false ;
 
+        while(x<=188){
+            sum =sum+x++;
+            if (sum >= 134 && Printed == false){
+                System.out.println("Sum bigger than 134 is " + sum + ", index is " + x );
+                Printed =true ;
+            }
+        }
+        System.out.println("Total is " + sum);
+    }
+}
 
 
 
