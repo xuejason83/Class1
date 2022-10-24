ecac97115b0d0a45da74e20c00bb92d076475423
Kevin Hong <kevinhongshenjie@gmail.com>
2022-10-23 21:03:15 -0400

    3rd gen Sum

--- MyFirstJavaProject/src/Sum.java
+++ MyFirstJavaProject/src/Sum.java
@@ -1,18 +1,30 @@
 public class Sum {
 	
     public static void main(String[] args) {
-        int x=1;
-        int sum=0;
+//        int x=1;
+//        int sum=0;
+//        boolean Printed = false ;
+//
+//        while(x<=188){
+//            sum =sum+x++;
+//            if (sum >= 134 && Printed == false){
+//                System.out.println("Sum bigger than 134 is " + sum + ", index is " + x );
+//                Printed =true ;
+//            }
+//        }
+//        System.out.println("Total is " + sum);
+        int sum =0;
         boolean Printed = false ;
-
-        while(x<=188){
-            sum =sum+x++;
-            if (sum >= 134 && Printed == false){
-                System.out.println("Sum bigger than 134 is " + sum + ", index is " + x );
-                Printed =true ;
-            }
+        for(int i=1;i<=100;i++) {        	
+        	sum += i; //sum = sum + i;
+          if (sum >= 134 && Printed == false){
+          System.out.println("Sum bigger than 134 is " + sum + ", index is " + x );
+          Printed =true ;
+      }
         }
-        System.out.println("Total is " + sum);
+        System.out.println(sum);
+        
+        
     }
 }
 
